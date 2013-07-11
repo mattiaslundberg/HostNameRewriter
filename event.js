ebdeb4c89941a8f80a0425c7cda4ffd16d8d690c
Mattias Lundberg <lundberg.mattias@gmail.com>
2013-07-11 22:10:47 +0200

    Add a browser action (does nothing at the moment)

--- event.js
+++ event.js
@@ -1,8 +1,8 @@
 chrome.webNavigation.onBeforeNavigate.addListener(function(event) {
 	chrome.storage.sync.get(function(items) {
 		for (from in items) {
-			var regex = new RegExp("^http[s]?\:\/\/" + from + "\/\.*$", "i");
-			if (event.url.match(regex)) {
+			var runOn = new RegExp("^http[s]?\:\/\/" + from + "\/\.*$", "i");
+			if (event.url.match(runOn)) {
 				chrome.tabs.get(event.tabId, function(tab) {
 					if (tab.url == event.url) { // Only update if main
 						var protore = new RegExp("^(http[s]?\:\/\/)\.*$", "i");
@@ -15,4 +15,13 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(event) {
 			}
 		}
 	});
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var runOn = new RegExp("^(http[s]?\:\/\/)\.*$", "i");
+	if (tab.url.match(runOn)) {
+		// Create and display the action
+	} else {
+		// PASS
+	}
 });

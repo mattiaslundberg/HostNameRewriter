f726679071683941fcf6139699b26caa7e1a8f0d
Mattias Lundberg <lundberg.mattias@gmail.com>
2013-10-06 13:44:07 +0200

    Make it possible to remove a rewrite in the browser action.

--- action.js
+++ action.js
@@ -11,6 +11,7 @@ chrome.tabs.getSelected(function(tab) {
 		for (var from in items) {
 			if (match[2] == items[from]) {
 				document.getElementById("information").innerHTML = "Current rule:";
+				document.getElementById("remove").style["display"] = "";
 				fromBox.value = from;
 				document.getElementById("to").value = items[from];
 				found = true;
@@ -19,6 +20,7 @@ chrome.tabs.getSelected(function(tab) {
 		}
 		if (!found) {
 			document.getElementById("information").innerHTML = "Add rule:";
+			document.getElementById("remove").style["display"] = "none";
 			fromBox.value = match[2];
 		}
 		fromBox.disabled = true;
@@ -30,16 +32,33 @@ function on_key_press(event) {
 		save_options();
 }
 
+function remove_rewrite() {
+	var from = document.getElementById("from").value;
+	chrome.storage.sync.remove(from);
+
+	// Go back to original.
+	chrome.tabs.getSelected(function(tab) {
+		var match = tab.url.match(regex);
+		chrome.tabs.update(tab.id, {url: match[1] + from + match[3]});
+		window.close();
+	});
+}
+
 function save_options() {
 	var from = document.getElementById("from").value;
 	var to = document.getElementById("to").value;
 	var store = {};
 	store[from] = to;
 	chrome.storage.sync.set(store);
+
+	// Use the rewrite to go to the new host.
 	chrome.tabs.getSelected(function(tab) {
 		var match = tab.url.match(regex);
 		chrome.tabs.update(tab.id, {url: match[1] + to + match[3]});
+		window.close();
 	});
 }
 
+
 document.querySelector("#save").addEventListener("click", save_options);
+document.querySelector("#remove").addEventListener("click", remove_rewrite);

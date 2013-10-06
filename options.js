c76f23e5174427c84fce65b41ae7a669ccc09a61
Mattias Lundberg <lundberg.mattias@gmail.com>
2013-10-06 13:07:43 +0200

    Add user notifications when adding or removing a rewrite rule (fix #2).

--- options.js
+++ options.js
@@ -1,14 +1,47 @@
+var hide_timeout;
+
+function hide_message() {
+	clearTimeout(hide_timeout);
+	var top = document.getElementById("title");
+	top.style["margin-top"] = 0;
+	var message = document.getElementById("message");
+	message.innerHTML = "";
+	message.style["top"] = "-160px";
+	message.className = "";
+}
+
+function show_message(type, header, text, time) {
+	hide_message();
+	var top = document.getElementById("title");
+	top.style["margin-top"] = "107px";
+	var message = document.getElementById("message");
+	var h = document.createElement("h3");
+	h.innerHTML = header;
+	message.appendChild(h);
+	var t = document.createElement("p");
+	t.innerHTML = text;
+	message.appendChild(t);
+	message.className = type;
+	message.style["top"] = "0";
+	hide_timeout = setTimeout(function() { hide_message(); }, time * 1000);
+}
+
+
 function restore_options() {
 	chrome.storage.sync.get(function(items) {
+		var len = 0;
 		for (var key in items) {
+			len++;
 			var item = items[key];
 			add_row(null, key, item);
 		}
-		if (items.length <= 0) {
+		if (len === 0) {
 			add_row(null, null, null);
+			show_message("info", "No rewrite rules added", "In order to use this extension you must add at least one rewrite rule.", 15);
 		}
 	});
 }
+
 function add_row(event, original, target) {
 	var table = document.getElementById("urls").children[1];
 	var rowCount = table.rows.length;
@@ -32,11 +65,14 @@ function add_row(event, original, target) {
 	el.id = "row" + rowCount;
 	cell.appendChild(el);
 }
+
 function del_row(event) {
 	var row = event.target.parentNode.parentNode;
 	chrome.storage.sync.remove(row.children[0].children[0].value);
 	row.parentNode.removeChild(row);
+	show_message("success", "Successfully removed", "The removed rewrite rule have been successfully removed.", 5);
 }
+
 function save_options() {
 	var rows = document.getElementById("urls").children[1].children;
 	var store = {};
@@ -52,7 +88,11 @@ function save_options() {
 		}
 	});
 	chrome.storage.sync.set(store);
+	show_message("success", "Successfully saved", "The entered rewrite rules are successfully saved.", 5);
 }
+
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
 document.querySelector('#add_url').addEventListener('click', add_row);
+document.querySelector('#message').addEventListener('click', hide_message);
